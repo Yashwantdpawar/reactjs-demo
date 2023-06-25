d4e7e15eaebc2d38f0137078f89fa52adf816aa8
Yashwantdpawar <yashwantdpawar@gmail.com>
2023-06-25 23:17:14 +0530

    test

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ docker build -t reactapp:latest .
 
 docker tag reactapp:latest ajaydhanvi17/reactapp:latest
 
-docker login --username ajaydhanvi17 --password Pritamaj8513@# 
+docker login --username foo --password bar
 
 docker push ajaydhanvi17/myreactapp:latest
